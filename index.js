3e2a0ea8da6c330c2d50d59f24c0fb848f28cc23
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2023-08-30 21:29:35 -0500

    progress made on the grid

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { stdout, stdin } = require("process");
 const { createMatrix, loadGrid } = require("./modules/bash-grid.js")
 
-loadGrid()
+loadGrid([[]])
 // stdout.moveCursor(0, -500)
 
 // while (true)
--- modules/bash-grid.js
+++ modules/bash-grid.js
@@ -6,13 +6,75 @@ readline.emitKeypressEvents(stdin);
 function createMatrix(x, y)
 {
     const matrix = new Array(y);
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < matrix.length; i++) {
         matrix[i] = new Array(x);
     } 
     return matrix
 }
 
 exports.createMatrix = createMatrix
+
+const tlCorner = "┌"
+const trCorner = "┐"
+const blCorner = "└"
+const brCorner = "┘"
+
+const vLine    = "│"
+const hLine    = "─"
+
+const topT     = "┴"
+const botttomT = "┬"
+const rightT   = "├"
+const leftT    = "┤"
+
+const _4 = "┼"
+
+function getEdge(x, y, width, height)
+{
+    // Edged
+    if (x == 0 && y == 0)
+        return tlCorner
+    if (x == 0 && y == height-1)
+        return blCorner
+    if (x == width-1 && y == 0)
+        return trCorner
+    if (x == width-1 && y == height-1)
+        return brCorner
+
+
+    if (x == 0)
+        return rightT
+    if (x == width-1)
+        return leftT
+    if (y == 0)
+        return botttomT
+    if (y == height-1)
+        return topT
+
+    return _4
+}
+
+function drawGrid(grid)
+{
+    const width = 10
+    const height = 10
+    
+    let gridStr = ""
+    const spacing = 1
+
+    for (let y = 0; y < height; y++)
+    {
+        // Line
+        for (let x = 0; x < width; x++)
+        {
+            gridStr += getEdge(x, y, width+1, height+1)
+        }
+        gridStr += getEdge(width+1, y, width+1, height+1)
+        gridStr += "\n"
+    }
+
+    return gridStr
+}
 
 function loadGrid(grid)
 {
@@ -27,8 +89,10 @@ function loadGrid(grid)
     stdin.setRawMode( true );
     stdin.resume();
 
+    const drawMatrix = createMatrix(grid.length, grid[0].length)
+
     stdin.on("keypress", onKeypress)
-    console.log("Listening")
+    console.log(drawGrid())
 }
 
 exports.loadGrid = loadGrid
